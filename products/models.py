3bca203b4f6de494c775d3bc5fc259098adedc19
Azat <azatrashov141102@gmail.com>
2022-11-17 03:29:11 +0600

    homework 3

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,6 +9,7 @@ class Category(models.Model):
         return self.title
 
 class Product(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField()
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -18,3 +20,10 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author.username}_{self.product}'
--- products/views.py
+++ products/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Comment
 # Create your views here.
 
-def product_detail_view(request, id):
-    dict_ = {}
-    product = Product.objects.get(id=id)
-    dict_['product_detail'] = product
-    return render(request, 'detail.html', context=dict_)
 
 def main_view(request):
     if request.method == 'GET':
         return render(request, 'layouts/main.html')
-
+def product_detail_view(request, id):
+    if request.method == "GET":
+        product = Product.objects.get(id=id)
+        comments = Comment.objects.filter(product_id=id)
+        data = {
+            'product': product,
+            'comments': comments
+        }
+    return render(request, 'products/detail.html', context=data)
 def product_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
