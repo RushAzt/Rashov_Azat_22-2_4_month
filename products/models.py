95beb9c3122601d6d6f51142e73d8ecd37f0b0ec
Azat <azatrashov141102@gmail.com>
2022-11-22 18:23:31 +0600

    homework 4

--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Category(models.Model):
+    icon = models.ImageField(null=True)
     title = models.CharField(max_length=100)
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from products.models import Product, Comment
+from products.models import Product, Comment, Category
 # Create your views here.
 
 
@@ -17,7 +17,22 @@ def product_detail_view(request, id):
     return render(request, 'products/detail.html', context=data)
 def product_view(request):
     if request.method == 'GET':
-        products = Product.objects.all()
+        category_id = request.GET.get('category_id')
+
+        if category_id:
+            products = Product.objects.filter(categories__in=[category_id])
+        else:
+            products = Product.objects.all()
+
+        products = [{
+            'id': product.id,
+            'title': product.title,
+            'image': product.image,
+            'description': product.description,
+            'price': product.price,
+            'amount': product.amount,
+            'categories': product.categories
+        } for product in products]
 
         data = {
             'products': products
@@ -25,7 +40,12 @@ def product_view(request):
 
         return render(request, 'products/products.html', context=data)
 
+
 def categories_view(request):
     if request.method == 'GET':
-        return render(request, 'categories/categories.html')
+        categories = Category.objects.all()
+        data = {
+            'categories': categories
+        }
+        return render(request, 'categories/categories.html', context=data)
 
